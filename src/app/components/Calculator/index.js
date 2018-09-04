d0d9e25d3cf3ed43a326c3437309bfe7952c29e0
Firanolfind <firanolfind@gmail.com>
2018-09-04 06:06:28 +0400

    calc error handler

--- src/app/components/Calculator/index.js
+++ src/app/components/Calculator/index.js
@@ -12,31 +12,29 @@ import Dot from './Dot';
 import Menu from './Menu';
 import style from './Calculator.sass';
 
-const Col5 = ({children, xs}) => (
-  <div className={classNames('col', 'col5', {
-    [`col5-${xs}`]: xs
-  })}>{children}</div>
+const Col5 = ({ children, xs }) => (
+  <div
+    className={classNames('col', 'col5', {
+      [`col5-${xs}`]: xs,
+    })}
+  >
+    {children}
+  </div>
 );
 
 class Calculator extends Component {
-  static propTypes = {
-  };
+  static propTypes = {};
 
   static defaultProps = {
     memory: false,
     error: false,
-    accumulator: [0]
-  }
+    accumulator: [0],
+  };
 
-  Button = (props) => <Btn onBtnClick={this.props.onBtnClick} {...props} />
+  Button = (props) => <Btn onBtnClick={this.props.onBtnClick} {...props} />;
 
   render() {
-    const {
-      memory,
-      error,
-      accumulator,
-      onOffClick
-    } = this.props;
+    const { memory, error, accumulator, onOffClick } = this.props;
 
     // const digits = this.props.digits.reverse();
 
@@ -44,98 +42,97 @@ class Calculator extends Component {
 
     const length = accumulator.length;
 
-    const number = length === 1 || length === 3 ? accumulator[length - 1] : length === 2 ? accumulator[0] : 0;
+    const number = length ? accumulator[length - 1] : 0;
     const minus = number < 0;
     const abs = Math.abs(number);
     const stringNum = abs.toString().split('.');
     const int = stringNum[0];
     const decimal = stringNum[1] || '';
     const dotPos = decimal ? decimal.length : 0;
-    const commaPos = [...Array(~~(int.length/3)).keys()]
-                        .filter(item => int.length > 3)
-                        .map(item => item * 3)
-                        .map(item => dotPos + item)
-                        .map(item => item + 2);
+    const commaPos = [...Array(~~(int.length / 3)).keys()]
+      .filter((item) => int.length > 3)
+      .map((item) => item * 3)
+      .map((item) => dotPos + item)
+      .map((item) => item + 2);
     const digits = (int + decimal)
-                      .split('')
-                      .reverse()
-                      .map(item => ~~item);
+      .split('')
+      .reverse()
+      .map((item) => ~~item);
 
     return (
       <Col xs="12">
         <Row className={style.Calculator}>
           <Col xs="12" className="header" />
           <Col xs="12" className="body">
-            <div className="logo">
-            </div>
+            <div className="logo" />
             <div className="solarPanel">
               <div className="cells">
-                <div className="cell"/>
-                <div className="cell"/>
-                <div className="cell"/>
-                <div className="cell"/>
+                <div className="cell" />
+                <div className="cell" />
+                <div className="cell" />
+                <div className="cell" />
               </div>
             </div>
-            <div className="slogan">
-              two way power
-            </div>
+            <div className="slogan">two way power</div>
             <Display>
-              {[...Array(8).keys()].reverse().map(item => (
-                <Digit position={item} key={item} number={digits[item]} />
-              ))}
-              {[...Array(8).keys()].reverse().map(item => (
-                <Dot position={item} key={item} on={dotPos && item === dotPos} />
-              ))}
-              {[...Array(7).keys()].reverse().map(item => (
-                <Comma position={item} key={item} on={commaPos.indexOf(item) > -1} />
-              ))}
+              {[...Array(8).keys()].reverse().map((item) => <Digit position={item} key={item} number={digits[item]} />)}
+              {[...Array(8).keys()]
+                .reverse()
+                .map((item) => <Dot position={item} key={item} on={dotPos && item === dotPos} />)}
+              {[...Array(7).keys()]
+                .reverse()
+                .map((item) => <Comma position={item} key={item} on={commaPos.indexOf(item) > -1} />)}
               <Menu minus={minus} memory={memory} error={error} />
             </Display>
             <Row className="buttonRow">
               <Col xs="7" className="modelName">
                 SL-300SV
               </Col>
-              <Button size="sm" name="root"/>
-              <Button name="off" size="sm" onBtnClick={onOffClick}>OFF</Button>
+              <Button size="sm" name="root" />
+              <Button name="off" size="sm" onBtnClick={onOffClick}>
+                OFF
+              </Button>
             </Row>
             <Row className="buttonRow">
               <Button name="memoryClear">MC</Button>
               <Button name="memoryRestore">MR</Button>
               <Button name="memoryMinus">M–</Button>
               <Button name="memoryPlus">M+</Button>
-              <Button name="divide"/>
+              <Button name="divide" />
             </Row>
             <Row className="buttonRow">
-              <Button name="percent"/>
-              <Button name="seven"/>
-              <Button name="eight"/>
-              <Button name="nine"/>
-              <Button name="multiply"/>
+              <Button name="percent" />
+              <Button name="seven" />
+              <Button name="eight" />
+              <Button name="nine" />
+              <Button name="multiply" />
             </Row>
             <Row className="buttonRow">
-              <Button name="plusminus"/>
-              <Button name="four"/>
-              <Button name="five"/>
-              <Button name="six"/>
-              <Button name="minus"/>
+              <Button name="plusminus" />
+              <Button name="four" />
+              <Button name="five" />
+              <Button name="six" />
+              <Button name="minus" />
             </Row>
             <Row>
               <Col5 xs="4">
                 <Row className="buttonRow">
-                  <Button name="C" color="red" w1_4/>
-                  <Button name="one" w1_4/>
-                  <Button name="two" w1_4/>
-                  <Button name="three" w1_4/>
+                  <Button name="C" color="red" w1_4 />
+                  <Button name="one" w1_4 />
+                  <Button name="two" w1_4 />
+                  <Button name="three" w1_4 />
                 </Row>
                 <Row className="buttonRow">
-                  <Button name="allClear" color="red" w1_4>AC</Button>
-                  <Button name="zero" w1_4/>
-                  <Button name="dot" w1_4/>
-                  <Button name="equal" w1_4/>
+                  <Button name="allClear" color="red" w1_4>
+                    AC
+                  </Button>
+                  <Button name="zero" w1_4 />
+                  <Button name="dot" w1_4 />
+                  <Button name="equal" w1_4 />
                 </Row>
               </Col5>
               <Col5 xs="1">
-                <Button name="plus" size="lg" w100/>
+                <Button name="plus" size="lg" w100 />
               </Col5>
             </Row>
           </Col>
--- src/redux/ducks/calculator/reducers.js
+++ src/redux/ducks/calculator/reducers.js
@@ -1,23 +1,26 @@
 import * as a from './constants';
-import Decimal from 'decimal';
+import Decimal from 'decimal.js';
+
+const limitsize = 8;
+const max = new Decimal([...Array(limitsize)].map(() => '9').join(''));
 
 const initialState = {
-    memory: null,
-    error: false,
-    minus: false,
-    number: 0,
-    accumulator: [],
-    memory: 0,
-    operator: null,
-    calculated: false
+  memory: null,
+  error: false,
+  minus: false,
+  number: 0,
+  accumulator: [],
+  memory: 0,
+  operator: null,
+  calculated: false,
 };
 
 export default (state = initialState, { type }) => {
-
   const { accumulator, operator, calculated } = state;
+  let error = false;
   const length = accumulator.length;
 
-  switch(type) {
+  switch (type) {
     case a.ZERO:
     case a.ONE:
     case a.TWO:
@@ -28,76 +31,115 @@ export default (state = initialState, { type }) => {
     case a.SEVEN:
     case a.EIGHT:
     case a.NINE: {
-
       const number = new Decimal(a.numbers[type]);
-
       if (calculated) {
         return {
           ...state,
           calculated: false,
-          accumulator: [number.toNumber()]
-        }
+          accumulator: [number.toNumber()],
+          error,
+        };
       }
-
-      if(length === 0 || (length === 1 && operator)) {
+      if (length === 0 || (length === 1 && operator)) {
         return {
           ...state,
           calculated: false,
-          accumulator: [...accumulator, number.toNumber()]
-        }
+          accumulator: [...accumulator, number.toNumber()],
+          error,
+        };
       }
 
       const str = accumulator[length - 1].toString();
+
+      if (str.length > limitsize - 1) {
+        return state;
+      }
+
       const value = str + a.numbers[type];
       accumulator[length - 1] = new Decimal(value).toNumber();
-
       return {
         ...state,
         calculated: false,
-        accumulator: [...accumulator]
-      }
+        accumulator: [...accumulator],
+        error,
+      };
     }
 
     case a.PLUS:
     case a.MINUS:
     case a.MULTIPLY:
     case a.DIVIDE: {
-
-      if(length > 1) {
-        const value = new Decimal(accumulator[0])[operator](accumulator[1]);
+      if (length > 1) {
+        let value = new Decimal(accumulator[0])[operator](accumulator[1]);
+        if (value.comparedTo(max) > 0) {
+          value = max;
+          error = true;
+        }
         return {
           ...state,
           calculated: false,
           accumulator: [value.toNumber()],
-          operator: a.operators[type]
-        }
+          operator: a.operators[type],
+          error,
+        };
       }
-
       return {
         ...state,
         calculated: false,
         accumulator: [...accumulator],
-        operator: a.operators[type]
-      }
+        operator: a.operators[type],
+        error,
+      };
     }
 
     case a.EQUAL: {
-
-      if(length > 1) {
-        const value = new Decimal(accumulator[0])[operator](accumulator[1]);
-
+      if (length > 1) {
+        let value = new Decimal(accumulator[0])[operator](accumulator[1]);
+        console.log(value.toNumber());
+        if (value.comparedTo(max) > 0) {
+          value = max;
+          error = true;
+        }
         return {
           ...state,
           calculated: true,
           accumulator: [value.toNumber()],
-          operator: null
-        }
+          operator: null,
+          error,
+        };
+      }
+      return state;
+    }
+
+    case a.ROOT: {
+      if (length === 1) {
+        const value = new Decimal(accumulator[0]).sqrt();
+        return {
+          ...state,
+          calculated: true,
+          accumulator: [value.toNumber()],
+          operator: null,
+          error,
+        };
       }
 
+      if (length > 1) {
+        let value = new Decimal(accumulator[0])[operator](accumulator[1]).sqrt();
+        if (value.comparedTo(max) > 0) {
+          value = max;
+          error = true;
+        }
+        return {
+          ...state,
+          calculated: true,
+          accumulator: [value.toNumber()],
+          operator: null,
+          error,
+        };
+      }
       return state;
     }
   }
 
   return state;
-}
-
+};
